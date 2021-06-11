35b042daa6bf19d8736f5e8af0519bc35c411f9c
Jhon Larru <larru93@gmailc.om>
2021-06-11 01:12:05 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+export interface AdminStore{
+    id?:number,
+    dni?:string,
+    name?:string,
+    zone_id?:number
+}
+export interface dniResponse{
+    primerNombre?:string,
+    segundoNombre?:string
+    apellidoPaterno?:string,
+    apellidoMaterno?:string
+}
+export interface ResponseOperation{
+    type?:string;
+    message?:string;
+    result?:any;
+}
+export interface Zone{
+    id?:number,
+    name?:string
+}
+export interface Store{
+    id?:number,
+    adminStore_id?:number,
+    name?:string,
+    addressStreet?:string,
+    location?:string,
+    brand_id?:number,
+    zone_id?:number
+}
+export interface Brand{
+    id?:number,
+    name?:string,
+    zone_id?:number
+}
